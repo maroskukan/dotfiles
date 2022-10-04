1a9262641843d55edc10a048832fa0f0e9f4343e
Maros Kukan <maros.kukan@me.com>
2022-10-04 15:49:39 +0200

    Remove wibar, update autorun

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -99,47 +99,47 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+--mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
-local taglist_buttons = gears.table.join(
-                    awful.button({ }, 1, function(t) t:view_only() end),
-                    awful.button({ modkey }, 1, function(t)
-                                              if client.focus then
-                                                  client.focus:move_to_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, function(t)
-                                              if client.focus then
-                                                  client.focus:toggle_tag(t)
-                                              end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
-                )
+--local taglist_buttons = gears.table.join(
+--                    awful.button({ }, 1, function(t) t:view_only() end),
+--                    awful.button({ modkey }, 1, function(t)
+--                                              if client.focus then
+--                                                  client.focus:move_to_tag(t)
+--                                              end
+--                                          end),
+--                    awful.button({ }, 3, awful.tag.viewtoggle),
+--                    awful.button({ modkey }, 3, function(t)
+--                                              if client.focus then
+--                                                  client.focus:toggle_tag(t)
+--                                              end
+--                                          end),
+--                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+--                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+--                )
 
-local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
+--local tasklist_buttons = gears.table.join(
+--                     awful.button({ }, 1, function (c)
+--                                              if c == client.focus then
+--                                                  c.minimized = true
+--                                              else
+--                                                  c:emit_signal(
+--                                                      "request::activate",
+--                                                      "tasklist",
+--                                                      {raise = true}
+--                                                  )
+--                                              end
+--                                          end),
+--                     awful.button({ }, 3, function()
+--                                              awful.menu.client_list({ theme = { width = 250 } })
+--                                          end),
+--                     awful.button({ }, 4, function ()
+--                                              awful.client.focus.byidx(1)
+--                                          end),
+--                     awful.button({ }, 5, function ()
+--                                             awful.client.focus.byidx(-1)
+--                                          end))
 
 local function set_wallpaper(s)
     -- Wallpaper
@@ -188,26 +188,26 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-     s.mywibox = awful.wibar({ position = "top", screen = s })
+--     s.mywibox = awful.wibar({ position = "top", screen = s })
 
     -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
-        },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
-            wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
-        },
-    }
+--    s.mywibox:setup {
+--        layout = wibox.layout.align.horizontal,
+--        { -- Left widgets
+--            layout = wibox.layout.fixed.horizontal,
+--            mylauncher,
+--            s.mytaglist,
+--            s.mypromptbox,
+--        },
+--        s.mytasklist, -- Middle widget
+--        { -- Right widgets
+--            layout = wibox.layout.fixed.horizontal,
+--            mykeyboardlayout,
+--            wibox.widget.systray(),
+--            mytextclock,
+--           s.mylayoutbox,
+--        },
+--    }
 end)
 -- }}}
 
@@ -563,14 +563,8 @@ beautiful.useless_gap=7
 
 
 -- Custom Autorun programs
-autorun = true
-autorunApps =
-{
-    "picom --config ~/.config/picom/picom.conf",
-    "feh --bg-fill --randomize ~/Media/wallpapers"
-}
-if autorun then
-    for app = 1, #autorunApps do
-        awful.util.spawn(autorunApps[app])
-    end
-end
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("nm-applet")
+awful.spawn.with_shell("kmix")
+awful.spawn.with_shell("$HOME/.config/polybar/launch.sh")
+awful.spawn.with_shell("feh --randomize --bg-fill $HOME/Media/wallpapers")
