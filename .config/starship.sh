1fea66a0cfd0aa575bf4acf9e6801abab98b5cd6
Maros Kukan <maros.kukan@me.com>
2022-11-28 15:19:02 +0100

    Fix LFILE name

--- .config/starship.sh
+++ .config/starship.sh
@@ -1,8 +1,8 @@
 # find out which distribution we are running on
-LFILE="/etc/*-release"
+LFILE="/etc/os-release"
 MFILE="/System/Library/CoreServices/SystemVersion.plist"
 if [[ -f $LFILE ]]; then
-  _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+  _distro=$(awk '/^ID=/' /etc/os-release | awk -F'=' '{ print tolower($2) }')
 elif [[ -f $MFILE ]]; then
   _distro="macos"
 fi
